c33bb87baf09a4c1d37e6c096cd6aa889baa982a
Sai Rithwik M <rithwiksaim@gmail.com>
2022-02-18 17:46:56 +0530

    Frontend code initialised

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import FileUpload from './components/FileUpload'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <FileUpload />
+      </header>
+    </div>
+  );
+}
+
+export default App;
