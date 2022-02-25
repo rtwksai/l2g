505e2ca2d7afb3e49de8e117dcd2559301923945
Karthik Udupa <43147419+k-udupa2000@users.noreply.github.com>
2022-02-25 11:10:24 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 
 import FileUpload from './components/FileUpload'
-
+import Checkout from './components/Checkout';
+// import AddressForm from './components/FileUpload';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <FileUpload />
+        <Checkout />
       </header>
     </div>
   );
