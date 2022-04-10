52ea40df857d08e903aee37cccebf500399b3576
Utkarsh Agarwal <utkarsh7877@gmail.com>
2022-04-10 19:38:36 +0530

    added code to convert the dict format

---
+++ backend/backend/dictConverter.py
@@ -0,0 +1,45 @@
+import xmltodict
+
+
+
+def parse_xml(file_location):
+    with open(file_location, 'r', encoding='utf-8') as file:
+        global_xml_contents = file.read()
+    converted_dict = xmltodict.parse(global_xml_contents)
+    return converted_dict
+
+
+def extract_table(table_od):
+    col_list = []
+    if(type(table_od["column"]) ==  type([]) ):
+        for i in table_od["column"]:
+            col_list.append({"col_name":i["@name"], "data_type" : i["dataType"] })
+            
+    else: 
+        col_list.append({"col_name":table_od["column"]["@name"], "data_type" : table_od["column"]["dataType"] })
+    return { table_od["@name"] : col_list}
+        
+
+def extract_xml(xml_dict):
+    new_dict = {}
+    len(xml_dict["dbtype"]["dbSchema"])
+    for j in xml_dict["dbtype"]["dbSchema"]:
+        list_tables = []
+        for i in j["table"]:
+            list_tables.append( extract_table(i))
+        new_dict[j["@name"]] = list_tables
+    return {xml_dict["dbtype"]["@name"] : new_dict}
+
+
+def extract_db(old_dict):
+    list_tables = []
+    for i in old_dict["dbSchema"]["table"]:
+        list_tables.append( extract_table(i))
+    
+    return {old_dict["dbSchema"]["@name"] : list_tables} 
+
+
+
+
+old_dict = parse_xml("C:/Users/Utkarsh Agarwal/Documents/GitHub/l2g/backend/test1.xml")
+new_dict = extract_xml(old_dict)
